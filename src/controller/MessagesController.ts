2b43d16b59753835a5a4198b1c5b768b3550a2f7
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-04-25 11:22:51 -0300

    controller and route messages ok

---
+++ src/controller/MessagesController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { MessagesService } from "../services/MessagesService";
+
+
+class MessagesController {
+
+  async create(req: Request, res: Response) {
+    const { admin_id, user_id, text } = req.body;
+
+    const messagesService = new MessagesService();
+
+    const message = await messagesService.create({admin_id, user_id, text});
+
+    return res.json(message);
+  }
+
+}
+
+export { MessagesController };
