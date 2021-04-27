6d68d6b22b6e99cb501c6747a0a214fa4547a9a1
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-04-27 14:18:52 -0300

    connections migration

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,5 @@
-import express from "express";
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
-import path from "path";
-
-import "./database";
-import { routes } from "./routes";
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, "..", "public")));
-app.set("views", path.join(__dirname, "..", "public"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
-
-app.get("/pages/client", (req, res) => {
-  return res.render("html/client.html")
-})
-
-const http = createServer(app);
-const io = new Server(http);
-
-io.on("connection", (socket: Socket) => {
-  console.log("Conectado", socket.id);
-})
-
-app.use(express.json());
-
-app.use(routes);
+import { http } from "./http";
+import "./websocket/client";
 
 http.listen(3333, () => console.log("Server on port 3333"));
 
